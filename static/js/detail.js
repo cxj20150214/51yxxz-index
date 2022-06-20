9fc191d39eaf00f146b6a098595b49c4f6b24a2e
cxj20150214 <848935682@qq.com>
2022-06-20 15:46:08 +0800

    620

---
+++ static/js/detail.js
@@ -0,0 +1,18 @@
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    logUrl: "../static/img/logo.png", //logo图片地址
+    shows: 1, //控制手机active
+    rate: 4.0,
+    gameList1: [],
+    gameList2: [],
+    gameList3: []
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
+})

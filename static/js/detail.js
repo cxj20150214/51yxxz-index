a7cfbc3fceb16d163c5b3181ff0cb66992a1d90c
cxj20150214 <848935682@qq.com>
2022-06-22 10:36:05 +0800

    0622.1035 更新

--- static/js/detail.js
+++ static/js/detail.js
@@ -4,8 +4,33 @@ var app = new Vue({
     logUrl: "../static/img/logo.png", //logo图片地址
     shows: 1, //控制手机active
     rate: 4.0,
-    gameList1: [],
+    gameList1: [{
+      imgUrl: '../static/img/20191219025933751.png',
+      link: '#',
+      rate: 4.0,
+      num: "150MB",
+      tit: '王者荣耀体验服手游v3.72.1.1',
+    }, {
+      imgUrl: '../static/img/20191219025933751.png',
+      link: '#',
+      num: "150MB",
+      rate: 4.0,
+      tit: '王者荣耀体验服手游v3.72.1.1',
+    }, {
+      imgUrl: '../static/img/20191219025933751.png',
+      link: '#',
+      rate: 4.0,
+      num: "150MB",
+      tit: '王者荣耀体验服手游v3.72.1.1',
+    }, {
+      imgUrl: '../static/img/20191219025933751.png',
+      link: '#',
+      num: "150MB",
+      rate: 4.0,
+      tit: '王者荣耀体验服手游v3.72.1.1',
+    }],
     gameList2: [],
+    gameList3: [],
     gameList4: [{
       imgUrl: '../static/img/20191219025933751.png',
       link: './sjyx/detail.html',
